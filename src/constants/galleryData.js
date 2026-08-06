8624c35591a3b8adfc8ee6970b21d531370f63e2
Aaditya <patilaadi2006@gmail.com>
2026-08-06 22:49:38 +0530

    organise gallery data

--- src/constants/galleryData.js
+++ src/constants/galleryData.js
@@ -1,8 +1,8 @@
 export const galleryImages = [
-  { src: "gallery-factory.png", label: "Main Factory Floor", delay: "stagger-1" },
-  { src: "gallery-press.png", label: "Press Shop", delay: "stagger-2" },
-  { src: "gallery-welding.png", label: "Welding Cell", delay: "stagger-3" },
-  { src: "gallery-quality.png", label: "Quality Lab", delay: "stagger-4" },
-  { src: "gallery-finished.png", label: "Finished Products", delay: "stagger-5" },
-  { src: "gallery-assembly.png", label: "Assembly Line", delay: "stagger-6" }
+  { src: "gallery/gallery-factory.png", label: "Main Factory Floor", delay: "stagger-1" },
+  { src: "gallery/gallery-press.png", label: "Press Shop", delay: "stagger-2" },
+  { src: "gallery/gallery-welding.png", label: "Welding Cell", delay: "stagger-3" },
+  { src: "gallery/gallery-quality.png", label: "Quality Lab", delay: "stagger-4" },
+  { src: "gallery/gallery-finished.png", label: "Finished Products", delay: "stagger-5" },
+  { src: "gallery/gallery-assembly.png", label: "Assembly Line", delay: "stagger-6" }
 ];
